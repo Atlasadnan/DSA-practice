e21b9a21c866f473493547f577485af2239e7286
Atlasadnan <53138605+Atlasadnan@users.noreply.github.com>
2022-08-31 23:21:20 +0530

    Time: 11 ms (51.85%), Space: 10.9 MB (37.04%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+  set<vector<int>>ans;
+    void recur(vector<int>& candidates, int target,vector<int>&cur,int l){
+         if(target==0 ){
+            ans.insert(cur);
+            return;
+        }
+        for(int i=l;i<candidates.size();i++){
+            if(candidates[i]>target){
+                continue;
+            }
+            if(i>l and candidates[i]==candidates[i-1]){
+                continue;
+            }
+            
+            cur.push_back(candidates[i]);
+            recur(candidates,target-candidates[i],cur,i+1);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int>cur;
+        sort(candidates.begin(),candidates.end());
+        recur(candidates,target,cur,0);
+        vector<vector<int>>ans2;
+        for(auto it:ans){
+            ans2.push_back(it);
+        }
+        return ans2;
+            
+    }
+};
